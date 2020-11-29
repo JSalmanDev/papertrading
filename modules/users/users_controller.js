77228cdd1df19380e92ab8fc22c5427386a93152
JSalmanDev <jamil@isalman.net>
2020-11-29 18:00:21 +0500

    balance reset api function

--- modules/users/users_controller.js
+++ modules/users/users_controller.js
@@ -8,6 +8,39 @@ class Users {
   
     constructor() { }
 
+    
+    balanceReset() {
+        return async (req, res) => {
+            let { user_id } = req.body;
+            if (!user_id) {
+                return res.status(400).send({ msg: 'Bad Request' });
+            }
+            
+            try {
+                const userExists = await userModel.findOne({ where: { id: user_id } });
+                if (userExists) {
+                    const result = await userExists.update({ balance: 400 });
+                    return jwt.sign({ id: userExists.id, balance: 400, firstName: userExists.first_name, lastName: userExists.last_name, email: userExists.email }, config.privateKey, { expiresIn: '30 days' }, function(err, token) {
+                        if (err) {
+                            console.log('Error in generating jwt token. ', err);
+                            return res.status(500).json({ msg: 'Internal Server Error', error: err });
+                        } else {
+                            return res.status(200).send({ 
+                                token: token,
+                                msg: 'Balance reset Successfully'
+                            });
+                        }
+                    });
+                } else {
+                    return res.status(404).send({ msg: 'No user found' });
+                }
+            } catch (error) {
+                console.log('Error in reset balance', error);
+                return res.status(500).send({ msg: 'Internal Server Error', error });
+            }
+        }
+    }
+    
     userLogin() {
         return async (req, res) => {
             let { email, password } = req.body;
